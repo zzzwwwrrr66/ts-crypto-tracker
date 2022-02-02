6d9fdd169c90a753812834185bd8b6bb12d2d4d0
wooram <zzzwwwrrr66@gmail.com>
2022-02-02 17:42:25 +0900

    <finish> 0202

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>,
   document.getElementById('root')
 );
